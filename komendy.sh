7b7ec88a3103e3739ec448be063132821aef35e1
dsw56039 <dsw56039@student.dsw.edu.pl>
2025-06-30 22:13:24 +0200

    Add files via upload

---
+++ komendy.sh
@@ -0,0 +1,53 @@
+git commit
+git commit
+git branch bugFix
+git checkout bugFix
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+git checkout -b bugFix
+git commit 
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
+git checkout c4
+git checkout c3
+git branch -f bugFix HEAD~2
+git branch -f main c6
+git checkout c1
+git reset HEAD~1
+git checkout pushed
+git revert HEAD
+git cherry-pick c3 c4 c7
+git rebase -i c1
+git branch -f bugFix main
+git checkout main
+git cherry-pick c4
+git rebase -i main
+git commit --amend
+git rebase -i main
+git branch -f main c3''
+git checkout main
+git cherry-pick c2
+git branch -f main c1
+git cherry-pick c2' c3
+git tag v0 c1
+git tag v1 c2
+git checkout c2
+git rebase main bugFix
+git rebase bugFix side
+git rebase side another
+git rebase another main
+git branch bugWork main^^2^
+git checkout one
+git cherry-pick C4 C3 C2
+git checkout two
+git cherry-pick C5 C4 C3 C2
+git branch -f three C2
+
+
+
+
